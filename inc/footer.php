8274c0af50fa8712beab0fca3ef80516c58a67a5
gizzrsh <gizzatullint@bk.ru>
2026-06-13 19:36:10 +0500

    Initial commit

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="footer__inner container">
+        <a href="" class="footer__link"></a>
+        <a href="" class="footer__link"></a>
+        <a href="" class="footer__link"></a>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/normalize.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>Ольга Бузмакова - Оренбургская художница</title>
+</head>
+<body>
+    <?php include './inc/header.php' ?>
+    <main class="main">
+        <section class="hero">
+            <div class="hero__inner container">
+                <h1 class="hero__title title">Немного о себе</h1>
+                <div class="hero__block">
+                    <img class="hero__img" src="./assets/img/author.png" alt="Изображение Ольги Бузмаковой">
+                    <div class="hero__content">
+                        <h2 class="hero__about">О художнике</h2>
+                        <p class="hero__biography">
+                            Ольга Александровна Бузмакова (род. в 1983 году) - оренбургская художница. В 2001 году окончила отделение графического дизайна Оренбургского художественного училища. Ольга создаёт интерьерные картины с использованием масляных или акриловых красок и гипсовые панно
+                        </p>
+                        <p class="hero__biography">
+                            Ольга - постоянная участница ярмарок, конкурсов и выставок. Её панно преобразили дома многих оренбуржцев. Каждый барельеф художницы уникален и неповторим как застывшее мгновение лета
+                        </p>
+                        <div class="hero__actions">
+                            <a href="#" class="hero__action btn">Витрина</a>
+                            <a href="#" class="hero__action btn">Мероприятия</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="map">
+            <div class="map__inner container">
+                <div style="position:relative;overflow:hidden; width: 100%;">
+                    <iframe 
+                        src="https://yandex.ru/map-widget/v1/?mode=search&ol=biz&oid=22075455217&ll=55.109298%2C51.772357&z=18.57&utm_source=maps&show=card" 
+                        width="100%" 
+                        height="450" 
+                        frameborder="0" 
+                        allowfullscreen="true"
+                        style="display: block;">
+                    </iframe>
+                </div>  
+            </div>
+        </section>
+    </main>
+    <?php include './inc/footer.php'; ?>
+</body>
+</html>

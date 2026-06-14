5299b76710aef437ab173d255151be30bf647417
gizzrsh <gizzatullint@bk.ru>
2026-06-14 23:34:25 +0500

    feat: Add yandex map

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="icon" href="favicon.ico">
     <link rel="apple-touch-icon" href="./img/favicons/apple.png">
     <link rel="manifest" href="site.webmanifest">
+    <script src="./assets/js/script.js"></script>
 </head>
 <body>
     <?php include './inc/header.php' ?>
@@ -36,17 +37,12 @@
         </section>
         <section class="map">
             <div class="map__inner container">
-                <div style="position:relative;overflow:hidden; width: 100%;">
-                    <iframe 
-                        src="https://yandex.ru/map-widget/v1/?mode=search&ol=biz&oid=22075455217&ll=55.109298%2C51.772357&z=18.57&utm_source=maps&show=card" 
-                        width="100%" 
-                        height="450" 
-                        frameborder="0" 
-                        allowfullscreen="true"
-                        style="display: block;"
-                        title="Yandex Map">
+                <div style="position:relative;overflow:hidden;">
+                    <a href="https://yandex.ru/maps/org/masterskaya_ya/22075455217/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Мастерская Я</a>
+                    <a href="https://yandex.ru/maps/48/orenburg/category/art_workshop/184105850/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:14px;">Художественная мастерская в Оренбурге</a>
+                    <iframe src="https://yandex.ru/map-widget/v1/?ll=55.108822%2C51.772390&mode=search&oid=22075455217&ol=biz&z=16.59&show=card" width="100%" height="400" frameborder="1" allowfullscreen="true" style="position:relative;">
                     </iframe>
-                </div>  
+                </div>
             </div>
         </section>
     </main>

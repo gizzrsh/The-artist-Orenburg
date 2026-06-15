391dfbeabc9769070a5e8958860ed4c2f8a3894d
gizzrsh <gizzatullint@bk.ru>
2026-06-15 22:12:13 +0500

    refactor: Updated the code for database

--- config/database.php
+++ config/database.php
@@ -1,23 +1,33 @@
 <?php
 
-// Localhost
-$pdo = new PDO(
-    'mysql:host=localhost;dbname=artist_site;charset=utf8mb4',
-    'root',
-    'dad102rus'
-);
-// Beget Хостинг
-// $pdo = new PDO(
-//     'mysql:host=localhost;dbname=gizzatrg_artsite;charset=utf8mb4',
-//     'gizzatrg_artsite',
-//     'Dad102russs'
-// );
-
-$pdo->setAttribute(
-    PDO::ATTR_ERRMODE,
-    PDO::ERRMODE_EXCEPTION
-);
-$pdo->setAttribute(
-    PDO::ATTR_DEFAULT_FETCH_MODE,
-    PDO::FETCH_ASSOC
-);
+if ($_SERVER['HTTP_HOST'] === 'gizzatrg.beget.tech') {
+    // Beget Хостинг
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=gizzatrg_artsite;charset=utf8mb4',
+        'gizzatrg_artsite',
+        'Dad102russs'
+    );
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+    $pdo->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
+} elseif ($_SERVER['HTTP_HOST'] === 'localhost:8080') {
+    // Localhost
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=artist_site;charset=utf8mb4',
+        'root',
+        'dad102rus'
+    );
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+    $pdo->setAttribute(
+        PDO::ATTR_DEFAULT_FETCH_MODE,
+        PDO::FETCH_ASSOC
+    );
+}

2597d22b14b3e7382158682b87250ec257cd99b2
gizzrsh <gizzatullint@bk.ru>
2026-07-15 14:26:23 +0500

    feat: added show password button

--- auth/template/auth.template.php
+++ auth/template/auth.template.php
@@ -30,7 +30,12 @@ unset($_SESSION['active_tab']);
                         </div>
                         <div class="form-group">
                             <label for="login-password">Пароль</label>
-                            <input type="password" id="login-password" name="password" required placeholder="••••••••">
+                            <div class="input-group">
+                                <input type="password" id="login-password" name="password" required autocomplete="new-password" placeholder="••••••••">
+                                <button type="button" id="togglePassword" data-toggle="login-password">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00000"><path d="M607.5-372.5Q660-425 660-500t-52.5-127.5Q555-680 480-680t-127.5 52.5Q300-575 300-500t52.5 127.5Q405-320 480-320t127.5-52.5Zm-204-51Q372-455 372-500t31.5-76.5Q435-608 480-608t76.5 31.5Q588-545 588-500t-31.5 76.5Q525-392 480-392t-76.5-31.5ZM214-281.5Q94-363 40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200q-146 0-266-81.5ZM480-500Zm207.5 160.5Q782-399 832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280q113 0 207.5-59.5Z"/></svg>
+                                </button>
+                            </div>
                             <?php if (!empty($_SESSION['errors']['password'])): ?>
                                 <span class="form-error"><?= htmlspecialchars($_SESSION['errors']['password']) ?></span>
                             <?php endif; ?>
@@ -69,7 +74,7 @@ unset($_SESSION['active_tab']);
                             <label for="register-password">Пароль</label>
                             <div class="input-group">
                                 <input type="password" id="register-password" name="password" required autocomplete="new-password" placeholder="••••••••">
-                                <button type="button" id="togglePassword">
+                                <button type="button" id="togglePassword" data-toggle="register-password">
                                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#00000"><path d="M607.5-372.5Q660-425 660-500t-52.5-127.5Q555-680 480-680t-127.5 52.5Q300-575 300-500t52.5 127.5Q405-320 480-320t127.5-52.5Zm-204-51Q372-455 372-500t31.5-76.5Q435-608 480-608t76.5 31.5Q588-545 588-500t-31.5 76.5Q525-392 480-392t-76.5-31.5ZM214-281.5Q94-363 40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200q-146 0-266-81.5ZM480-500Zm207.5 160.5Q782-399 832-500q-50-101-144.5-160.5T480-720q-113 0-207.5 59.5T128-500q50 101 144.5 160.5T480-280q113 0 207.5-59.5Z"/></svg>
                                 </button>
                             </div>
